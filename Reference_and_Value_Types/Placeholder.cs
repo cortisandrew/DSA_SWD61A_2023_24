276443bb88882c459119be8c4bafca276aa97972
andrewc <andrewc@M201-T>
2024-02-09 15:54:19 +0100

    Add project files.

---
+++ Reference_and_Value_Types/Placeholder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reference_and_Value_Types
+{
+    public class Placeholder
+    {
+        public int Value { get; set; }
+        public Placeholder(int value) {
+            this.Value = value;
+        }
+    }
+}
---
+++ Reference_and_Value_Types/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Reference_and_Value_Types;
+
+// Value Types!
+int a = 5;
+int b = a; // 5
+
+a = 6;
+
+Console.WriteLine($"The value of a is {a}");    // 6
+Console.WriteLine($"The value of b is {b}");    // 5
+
+Console.WriteLine();
+Console.WriteLine();
+
+// Reference Types
+
+Placeholder pa = new Placeholder(5);    // 5
+Placeholder pb = pa;                    // 5
+
+pa.Value = 6;
+
+Console.WriteLine($"The value of pa is {pa.Value}");    // 6
+Console.WriteLine($"The value of pb is {pb.Value}");    // 6 
+
+
+
