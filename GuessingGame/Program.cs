3febe47813789cb137a88ed68b5605e2dc7df396
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2024-02-16 16:02:54 +0100

    Updated program

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,15 +1,27 @@
 ﻿using GuessingGame;
 
+List<int> problemSizes = new List<int>() { 100, 1000, 10000, 100000 };
 int REPETITIONS = 1000;
 
-for (int i = 0; i < REPETITIONS; i++)
+Console.WriteLine("Algorithm A:");
+foreach (int problemSize in problemSizes)
 {
-    SecretNumber secret = new SecretNumber(100);
-    AlgorithmA algorithmA = new AlgorithmA(secret);
-    var algorithmResults = algorithmA.Guess();
+    List<int> workCarriedOut = new List<int>(REPETITIONS);
 
+    for (int i = 0; i < REPETITIONS; i++)
+    {
+        SecretNumber secret = new SecretNumber(problemSize);
+        AlgorithmA algorithmA = new AlgorithmA(secret);
+        var algorithmResults = algorithmA.Guess();
 
-    Console.WriteLine($"Algorithm A found the secret number {algorithmResults.Item1} and used {algorithmResults.Item2} guesses");
+        workCarriedOut.Add(algorithmResults.Item2);
+        //Console.WriteLine($"Algorithm A found the secret number {algorithmResults.Item1} and used {algorithmResults.Item2} guesses");
+    }
+
+    //Console.WriteLine($"The fastest game took {workCarriedOut.Min()} guesses");
+    Console.WriteLine($"For a problem size {problemSize}, the mean number of guesses is {workCarriedOut.Average()} guesses");
+    // Console.WriteLine($"The slowest game took {workCarriedOut.Max()} guesses");
+
 }
 
 Console.ReadLine();
