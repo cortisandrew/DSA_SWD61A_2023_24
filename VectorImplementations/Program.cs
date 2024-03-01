2e6fd460c800f035acb24df4ba0a77128c7763b9
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2024-03-01 15:51:10 +0100

    Program tests speed of ABV (empirical analysis). Added stack.

--- VectorImplementations/Program.cs
+++ VectorImplementations/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
 using VectorImplementations;
 
+/*
 Console.WriteLine("Array Based Vector");
 
 
@@ -20,4 +22,117 @@ arrayBasedVector.InsertAtRank(3, "C#");
 
 Console.WriteLine(arrayBasedVector.RemoveAtRank(3));
 Console.WriteLine();
+*/
+
+ArrayBasedVector<int> abv = new ArrayBasedVector<int>();
+
+Stopwatch sw = new Stopwatch();
+
+List<int> problemSizes = new List<int>() { 100, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000 };
+int repetitions = 100;
+
+bool firstRun = true;
+
+Console.WriteLine("Get at Rank");
+foreach (int problemSize in problemSizes)
+{
+    // reset the stopwatch to 0 for the new problem size
+    sw.Reset();
+
+    abv = new ArrayBasedVector<int>(4); // new array based vector for this problem size, with array already large
+
+    // fill up the array to obtain the required problemSize
+    for (int i = 0; i < problemSize; i++)
+    {
+        abv.Append(i);
+    }
+    // now we have an array as large as the problem size!
+
+    for (int i = 0; i < repetitions; i++)
+    {
+        
+        sw.Start();
+        abv.ElementAtRank(1);
+        sw.Stop();
+    }
+
+    if (firstRun)
+    {
+        firstRun = false;
+    }
+    else
+    {
+        Console.WriteLine($"{problemSize}, {(double)sw.ElapsedTicks / repetitions}");
+    }
+}
+
+firstRun = true;
+Console.WriteLine("Append");
+foreach (int problemSize in problemSizes)
+{
+    // reset the stopwatch to 0 for the new problem size
+    sw.Reset();
+
+    abv = new ArrayBasedVector<int>(problemSize + repetitions + 1); // new array based vector for this problem size, with array already large
+    // length of array is a bit larger to avoid array growth
+
+    // fill up the array to obtain the required problemSize
+    for (int i = 0; i < problemSize; i++)
+    {
+        abv.Append(i);
+    }
+    // now we have an array as large as the problem size!
+
+    for (int i = 0; i < repetitions; i++)
+    {
+        sw.Start();
+        abv.Append(100);
+        sw.Stop();
+    }
+
+    if (firstRun)
+    {
+        firstRun = false;
+    }
+    else
+    {
+        Console.WriteLine($"{problemSize}, {(double)sw.ElapsedTicks / repetitions}");
+    }
+}
+
+firstRun = true;
+Console.WriteLine("Insert at 0");
+foreach (int problemSize in problemSizes)
+{
+    // reset the stopwatch to 0 for the new problem size
+    sw.Reset();
+
+    abv = new ArrayBasedVector<int>(problemSize + repetitions + 1); // new array based vector for this problem size, with array already large
+    // length of array is a bit larger to avoid array growth
+
+    // fill up the array to obtain the required problemSize
+    for (int i = 0; i < problemSize; i++)
+    {
+        abv.Append(i);
+    }
+    // now we have an array as large as the problem size!
+
+    for (int i = 0; i < repetitions; i++)
+    {
+        sw.Start();
+        abv.InsertAtRank(0, 100);
+        sw.Stop();
+    }
+
+    if (firstRun)
+    {
+        firstRun = false;
+    }
+    else
+    {
+        Console.WriteLine($"{problemSize}, {(double)sw.ElapsedTicks / repetitions}");
+    }
+}
+
+
 
---
+++ VectorImplementations/Stack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorImplementations
+{
+    public class Stack<T>
+    {
+        private ArrayBasedVector<T> abv;
+        public Stack(int defaultLength = 128) {
+            abv = new ArrayBasedVector<T>(defaultLength);
+        }
+        public void Push(T elem)
+        {
+            abv.Append(elem);
+        }
+
+        public T Pop()
+        {
+            return abv.RemoveAtRank(Size - 1);
+        }
+
+        public T Peek()
+        {
+            return abv.ElementAtRank(Size - 1);
+        }
+
+        public int Size
+        {
+            get
+            {
+                return abv.Size;
+            }
+        }
+    }
+}
