4fe788a13ca40ff7e5aa792d743a1955a894a9f8
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2024-03-11 10:14:05 +0100

    Added a new Stack.

---
+++ VectorImplementations/StackUsingSinglyLinkedLists.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorImplementations
+{
+    public class StackUsingSinglyLinkedLists<T> : IStackADT<T>
+    {
+        private SinglyLinkedList<T> linkedList = new SinglyLinkedList<T>();
+
+        public int Size
+        {
+            get
+            {
+                return linkedList.Size;
+            }
+        }
+
+        public T Peek()
+        {
+            return linkedList.GetFirstElement();
+        }
+
+        public T Pop()
+        {
+            return linkedList.RemoveLast();
+            //return linkedList.RemoveHead();
+        }
+
+        public void Push(T elem)
+        {
+            linkedList.Append(elem);
+            //linkedList.InsertFirst(elem);
+        }
+    }
+}
